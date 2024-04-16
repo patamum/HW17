6c5ad35a5787ced5dad4b9533a1594fa385db6c4
Svetlana Jourkova <belkina_s@mail.ru>
2024-04-16 23:37:21 +0300

    Добавьте файлы проекта.

---
+++ Task17_1/BankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task17_1
+{
+    class BankAccount<T>
+    {
+        public T Number {  get; set; }
+        public double Balance { get; set; }
+        public string Fio {  get; set; }
+
+        public string GetInfo()
+        {
+            Random random = new Random();
+            double Balance = random.Next(0, 1000000);
+            return $"Номер счета: {Number} ФИО: {Fio} Баланс: {Balance}"; 
+        }
+        public void Input()
+        {           
+            Console.WriteLine("Введите номер счета");
+            Number = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+            
+            Console.WriteLine("Введите ФИО");
+            Fio = Console.ReadLine();
+        }
+    }
+}
---
+++ Task17_1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task17_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount<int> bankAccount1 = new BankAccount<int>();
+            bankAccount1.Input();
+            Console.WriteLine(bankAccount1.GetInfo());
+
+            BankAccount<string> bankAccount2 = new BankAccount<string>();
+            bankAccount2.Input();
+            Console.WriteLine(bankAccount2.GetInfo());
+        }
+    }
+}
